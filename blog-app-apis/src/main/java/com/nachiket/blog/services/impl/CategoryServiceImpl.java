a0e11e98781d677b6b7de3009a0f364ea8e356a5
Nachiket <nachiketkhule.nk@gmail.com>
2022-12-15 23:10:02 +0530

    Category Entity, Dto, Repo, Service and Impl added

---
+++ blog-app-apis/src/main/java/com/nachiket/blog/services/impl/CategoryServiceImpl.java
@@ -0,0 +1,33 @@
+package com.nachiket.blog.services.impl;
+
+import com.nachiket.blog.payloads.CategoryDto;
+import com.nachiket.blog.services.CategoryService;
+
+import java.util.List;
+
+public class CategoryServiceImpl extends CategoryService {
+    @Override
+    public CategoryDto createCategory(CategoryDto categoryDto) {
+        return null;
+    }
+
+    @Override
+    public CategoryDto updateCategory(CategoryDto categoryDto, Integer categoryId) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategory(Integer categoryId) {
+
+    }
+
+    @Override
+    public CategoryDto getCategory(Integer categoryId) {
+        return null;
+    }
+
+    @Override
+    public List<CategoryDto> getCategory() {
+        return null;
+    }
+}
