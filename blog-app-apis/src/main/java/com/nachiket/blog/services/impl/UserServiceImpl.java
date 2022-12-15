21919679c2bdf470a83d705fa47d540539ed874a
Nachiket <nachiketkhule.nk@gmail.com>
2022-12-15 19:20:17 +0530

    Model Mapper used for DTO to Entity

--- blog-app-apis/src/main/java/com/nachiket/blog/services/impl/UserServiceImpl.java
+++ blog-app-apis/src/main/java/com/nachiket/blog/services/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.nachiket.blog.exception.ResourceNotFoundException;
 import com.nachiket.blog.payloads.UserDto;
 import com.nachiket.blog.repositories.UserRepo;
 import com.nachiket.blog.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,8 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepo userRepo;
+    @Autowired
+    private ModelMapper modelMapper;
     @Override
     public UserDto createUser(UserDto userDto) {
 
@@ -65,22 +68,29 @@ public class UserServiceImpl implements UserService {
     }
 
     public User dtoToUser(UserDto userDto){
-        User user = new User();
-        user.setId(userDto.getId());
-        user.setName(userDto.getName());
-        user.setEmail(userDto.getEmail());
-        user.setAbout(userDto.getAbout());
-        user.setPassword(userDto.getPassword());
-        return  user;
+//        User user = new User();
+//        user.setId(userDto.getId());
+//        user.setName(userDto.getName());
+//        user.setEmail(userDto.getEmail());
+//        user.setAbout(userDto.getAbout());
+//        user.setPassword(userDto.getPassword());
+//        return  user;
+        //MODEL MAPPER DEPENDENCIES ADDED
+        User user = this.modelMapper.map(userDto, User.class);
+
+        return user;
     }
     public UserDto userToDto(User user){
-        UserDto userDto = new UserDto();
-        userDto.setId(user.getId());
-        userDto.setName(user.getName());
-        userDto.setEmail(user.getEmail());
-        userDto.setAbout(user.getAbout());
-        userDto.setPassword(user.getPassword());
-        return userDto;
+//        UserDto userDto = new UserDto();
+//        userDto.setId(user.getId());
+//        userDto.setName(user.getName());
+//        userDto.setEmail(user.getEmail());
+//        userDto.setAbout(user.getAbout());
+//        userDto.setPassword(user.getPassword());
+//        return userDto;
+        //MODEL MAPPER DEPENDENCIES ADDED
+        UserDto userDto = this.modelMapper.map(user, UserDto.class);
+        return  userDto;
     }
 
 }
