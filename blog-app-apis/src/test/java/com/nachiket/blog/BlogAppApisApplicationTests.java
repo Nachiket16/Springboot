d7b1279035557a800ca0cd55b859cf595e340868
Nachiket <nachiketkhule.nk@gmail.com>
2022-12-15 13:00:11 +0530

    Repository class testing

--- blog-app-apis/src/test/java/com/nachiket/blog/BlogAppApisApplicationTests.java
+++ blog-app-apis/src/test/java/com/nachiket/blog/BlogAppApisApplicationTests.java
@@ -1,13 +1,25 @@
 package com.nachiket.blog;
 
+import com.nachiket.blog.repositories.UserRepo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogAppApisApplicationTests {
 
+	@Autowired
+	private UserRepo userRepo;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void repoTest(){
+		String className = this.userRepo.getClass().getName();
+		String pkgName = this.userRepo.getClass().getPackageName();
+		System.out.println(className);
+		System.out.println(pkgName);
 	}
 
 }
