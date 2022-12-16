8a22f34335b9e930b1f564cf47f28aff8d3c2a94
Nachiket <nachiketkhule.nk@gmail.com>
2022-12-16 14:08:47 +0530

    Post entity created with 2 reference to user and category

---
+++ blog-app-apis/src/main/java/com/nachiket/blog/entities/Post.java
@@ -0,0 +1,33 @@
+package com.nachiket.blog.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "post")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer postId;
+    @Column(name = "post_title", length = 100, nullable = false)
+    private String title;
+    @Column(length = 10000)
+    private String content;
+    private String imageName;
+    private Date addedDate;
+
+    //Many Post have one category
+    @ManyToOne
+    private Category category;
+
+    //Many Post can be written by a one user
+    @ManyToOne
+    private User user;
+}
