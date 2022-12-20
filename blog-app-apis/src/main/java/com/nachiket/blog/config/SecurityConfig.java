9fb786a3cb9f1b1e14f4c7f01546846412c8139e
Nachiket <nachiketkhule.nk@gmail.com>
2022-12-20 10:28:17 +0530

    Basic Authentication is added-> JS

--- blog-app-apis/src/main/java/com/nachiket/blog/config/SecurityConfig.java
+++ blog-app-apis/src/main/java/com/nachiket/blog/config/SecurityConfig.java
@@ -1,11 +1,25 @@
 package com.nachiket.blog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+@Configuration      //Used to declare bean
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+            http
+            .csrf().disable()
+            .authorizeHttpRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+
+    }
 }
