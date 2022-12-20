9d3d7bd8f9a4c40bd738248e217fb70f22ab8a6e
Nachiket <nachiketkhule.nk@gmail.com>
2022-12-20 17:01:21 +0530

    Role based Authenticaiton, Api access added

--- blog-app-apis/src/main/java/com/nachiket/blog/controllers/UserController.java
+++ blog-app-apis/src/main/java/com/nachiket/blog/controllers/UserController.java
@@ -7,6 +7,7 @@ import com.nachiket.blog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -36,6 +37,7 @@ public class UserController {
 
     //Delete -
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponse> deleteUser(@Valid @PathVariable("userId") Integer uid){
         this.userService.deleteUser(uid);
