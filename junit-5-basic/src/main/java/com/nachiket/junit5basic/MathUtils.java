56bd51859e91e6962f1a6b2e2fa39890ce5b2587
Nachiket <nachiketkhule.nk@gmail.com>
2022-12-22 15:59:42 +0530

    few unit testing done

--- junit-5-basic/src/main/java/com/nachiket/junit5basic/MathUtils.java
+++ junit-5-basic/src/main/java/com/nachiket/junit5basic/MathUtils.java
@@ -4,4 +4,13 @@ public class MathUtils {
     public int add(int a, int b){
         return a+b;
     }
+
+    public double computeCircleArea(double radius){
+        return Math.PI*Math.pow(radius,2);
+    }
+
+    public double divide(int a, int b){
+        return a/b;
+    }
+
 }
--- junit-5-basic/src/test/java/com/nachiket/junit5basic/MathUtilsTest.java
+++ junit-5-basic/src/test/java/com/nachiket/junit5basic/MathUtilsTest.java
@@ -6,12 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
 
+    MathUtils mathUtils = new MathUtils();
+
     @Test
-    void test(){
-        MathUtils mathUtils = new MathUtils();
-        int exp = 2;
+    void testAdd(){
+//        MathUtils mathUtils = new MathUtils();
+        int exp = 6;
         int act = mathUtils.add(5,1);
-        assertEquals(exp, act);
+        assertEquals(exp, act, "The add method should add two numbers");
+    }
+
+    @Test
+    void testCircleArea(){
+//        MathUtils mathUtils = new MathUtils();
+        double exp = 314.15926535897932384626433832795;
+        double act = mathUtils.computeCircleArea(10);
+        assertEquals(exp,act);
+    }
+
+    @Test
+    void testDivide(){
+//        MathUtils mathUtils = new MathUtils();
+        assertThrows(ArithmeticException.class, ()-> mathUtils.divide(5,0), "Divide by zero");
     }
 
 }
