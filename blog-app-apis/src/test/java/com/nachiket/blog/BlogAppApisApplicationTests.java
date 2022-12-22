e241bd06bbe15a968511b1006e6a783c3c43e09f
Nachiket <nachiketkhule.nk@gmail.com>
2022-12-22 11:45:26 +0530

    Unit testing

--- blog-app-apis/src/test/java/com/nachiket/blog/BlogAppApisApplicationTests.java
+++ blog-app-apis/src/test/java/com/nachiket/blog/BlogAppApisApplicationTests.java
@@ -4,22 +4,39 @@ import com.nachiket.blog.repositories.UserRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class BlogAppApisApplicationTests {
 
 	@Autowired
 	private UserRepo userRepo;
+
+	private Calculator c = new Calculator();
 	@Test
 	void contextLoads() {
 	}
 
+//	@Test
+//	public void repoTest(){
+//		String className = this.userRepo.getClass().getName();
+//		String pkgName = this.userRepo.getClass().getPackageName();
+//		System.out.println(className);
+//		System.out.println(pkgName);
+//	}
+
 	@Test
-	public void repoTest(){
-		String className = this.userRepo.getClass().getName();
-		String pkgName = this.userRepo.getClass().getPackageName();
-		System.out.println(className);
-		System.out.println(pkgName);
+	void testSum(){
+		//Expected Result
+		int expectedResult = 20;
+		//Actual Result
+		int res = c.doSum(12,3,5);
+
+		//We will use Assertion library
+		//import static org.assertj.core.api.Assertions.*;
+		assertThat(res).isEqualTo(expectedResult);
+
 	}
+
 
 }
---
+++ blog-app-apis/src/test/java/com/nachiket/blog/Calculator.java
@@ -0,0 +1,13 @@
+package com.nachiket.blog;
+
+public class Calculator {
+    //Sum
+    public int doSum(int a, int b, int c){
+        return a+b+c;
+    }
+
+    //Multiplication
+    public int doProduct(int a, int b){
+        return a*b;
+    }
+}
