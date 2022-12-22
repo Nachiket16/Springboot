57f30416cd05b38bd036013743698a04b96cc7ed
Nachiket <nachiketkhule.nk@gmail.com>
2022-12-22 18:52:47 +0530

    Nested Testing done

---
+++ junit-5-basic/Nachiket.md
@@ -0,0 +1,40 @@
+# Condition Execution
+>@EnabledOnOs(OS.LINUX)
+> 
+> @EnabledOnJre(JRE.JAVA_11)
+> 
+> @EnabledIf
+> 
+> @EnabledIfSystemProperty
+> 
+> @EnabledIfEnvironmentVariable
+
+# Handling External Factors
+
+>### assumethat
+> >same structure as assert.
+> 
+> eg: assumeTrue(value)
+
+
+# Assert
+
+>This contains many predefined methods
+> 
+> * assertEquals(a,b,"message");
+> * assertAll()
+> * assertThrow()
+
+### assertAll()
+@Test   
+@DisplayName("Multiply method test")   
+void testMultiply(){   
+assertAll(   
+() -> assertEquals(4, mathUtils.multiply(2,2)),  
+() -> assertEquals(0, mathUtils.multiply(0, 123)),  
+() -> assertEquals(-1, mathUtils.multiply(-1, 1))  
+);  
+}
+
+### @Nested
+It's used for nesting multiple test 
--- junit-5-basic/src/test/java/com/nachiket/junit5basic/MathUtilsTest.java
+++ junit-5-basic/src/test/java/com/nachiket/junit5basic/MathUtilsTest.java
@@ -1,14 +1,46 @@
 package com.nachiket.junit5basic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)  //If this is used then no need to make static before all method . Else you have to make BeforeAll as a static
 class MathUtilsTest {
+    MathUtils mathUtils;
 
-    MathUtils mathUtils = new MathUtils();
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("This needs to run before all");
+    }
+
+    @BeforeEach
+    void init(){
+        mathUtils = new MathUtils();
+    }
+
+    @Nested
+    class AddTest{
+        @Test
+        @DisplayName("Testing add for Positive No")
+        void testAddPositive(){
+            assertEquals(2, mathUtils.add(1,1), "The add method should add two numbers");
+        }
+        @Test
+        @DisplayName("Testing add for Negative No")
+        void testAddNegative(){
+            assertEquals(-2, mathUtils.add(-1,-1), "The add method should add two numbers");
+        }
+    }
+
+
+    @AfterEach
+    void cleanup(){
+        System.out.println("Cleaning up... ");
+    }
 
     @Test
+    @DisplayName("Testing add method")
     void testAdd(){
 //        MathUtils mathUtils = new MathUtils();
         int exp = 6;
@@ -27,7 +59,19 @@ class MathUtilsTest {
     @Test
     void testDivide(){
 //        MathUtils mathUtils = new MathUtils();
+        boolean isServerUp = false;
+        assumeTrue(isServerUp);    //This is for the external factor eg-> if server is down then test will fail. It will assume the server is running if not then will skip the test.
         assertThrows(ArithmeticException.class, ()-> mathUtils.divide(5,0), "Divide by zero");
+    }
+
+    @Test
+    @DisplayName("Multiply method test")
+    void testMultiply(){
+        assertAll(
+                () -> assertEquals(4, mathUtils.multiply(2,2)),
+                () -> assertEquals(0, mathUtils.multiply(0, 123)),
+                () -> assertEquals(-1, mathUtils.multiply(-1, 1))
+        );
     }
 
 }
