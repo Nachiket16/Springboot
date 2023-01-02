e06460192bbbe4423e836b1d1d834aede226c90b
nachiket.khule <nachiketkhule.nk@gmail.com>
2023-01-02 18:46:13 +0530

    Embedding class into the student

---
+++ spring-data-jpa/src/main/java/com/nachiketjpa/jpa/entity/Guardian.java
@@ -0,0 +1,8 @@
+package com.nachiketjpa.jpa.entity;
+
+public class Guardian {
+    //Embedding class into the student
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
--- spring-data-jpa/src/test/java/com/nachiketjpa/jpa/repository/StudentRepositoryTest.java
+++ spring-data-jpa/src/test/java/com/nachiketjpa/jpa/repository/StudentRepositoryTest.java
@@ -1,7 +1,37 @@
 package com.nachiketjpa.jpa.repository;
 
+import com.nachiketjpa.jpa.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest         //This will impact the DB
+//@DataJpaTest          //This will not impact the DB
 class StudentRepositoryTest {
+    @Autowired
+    private StudentRepository studentRepository;
 
+    @Test
+    public void saveStudent(){
+        Student student = Student.builder()
+                .emailId("nachiket@gmail.com")
+                .firstName("Nachiket")
+                .lastName("Khule")
+                .guardianName("Madhusudan")
+                .guardianEmail("madhu@yahoo.in")
+                .guardianMobile("12312312312")
+                .build();
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudent(){
+        List<Student> studentList = studentRepository.findAll();
+        System.out.println("StudentList = " + studentList);
+    }
 }
