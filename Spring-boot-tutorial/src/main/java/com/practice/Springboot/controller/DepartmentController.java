f02f8a7fde333f4de28bed3e85bc2c7780712c17
nachiket.khule <nachiketkhule.nk@gmail.com>
2023-01-02 10:45:13 +0530

    Logger Added

--- Spring-boot-tutorial/src/main/java/com/practice/Springboot/controller/DepartmentController.java
+++ Spring-boot-tutorial/src/main/java/com/practice/Springboot/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.practice.Springboot.controller;
 
 import com.practice.Springboot.entitiy.Department;
 import com.practice.Springboot.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,20 +16,28 @@ public class DepartmentController {
     @Autowired
     DepartmentService departmentService;
 
-    
+    //Logger is used for debugging the code
+    //Spring boot comes with SLF4J library by default
+    //For other library u have to add them into the pom.xml
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
 
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return this.departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
-         return this.departmentService.getAllDepartments();
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
+        return this.departmentService.getAllDepartments();
     }
 
     @GetMapping("/department/{departmentId}")
     public Department fetchDepartmentById(@PathVariable Long departmentId){
+        LOGGER.info("Inside fetchDepartmentById of DepartmentController");
         return this.departmentService.getDepartmentById(departmentId);
     }
 
