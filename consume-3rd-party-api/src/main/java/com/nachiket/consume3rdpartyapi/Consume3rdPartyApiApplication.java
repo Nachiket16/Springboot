4110328608a7fe681382950c13ddc38b4ecaa20e
nachiket.khule <nachiketkhule.nk@gmail.com>
2023-01-04 12:32:25 +0530

    Consumed using the HTTPRequest.newBuilder method

--- consume-3rd-party-api/src/main/java/com/nachiket/consume3rdpartyapi/Consume3rdPartyApiApplication.java
+++ consume-3rd-party-api/src/main/java/com/nachiket/consume3rdpartyapi/Consume3rdPartyApiApplication.java
@@ -3,11 +3,28 @@ package com.nachiket.consume3rdpartyapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @SpringBootApplication
 public class Consume3rdPartyApiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication.run(Consume3rdPartyApiApplication.class, args);
+
+		var url = "https://dummy.restapiexample.com/api/v1/employees";
+		//httprequest
+
+		var request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+		var client = HttpClient.newBuilder().build();
+		var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+		System.out.println(response.statusCode());
+		System.out.println(response.body());
+
 	}
 
 }
