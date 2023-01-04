8614406fd9e28cc97fccd298cc55945fe67b2acd
nachiket.khule <nachiketkhule.nk@gmail.com>
2023-01-04 11:46:25 +0530

    3rd party api consumption practice

---
+++ consume-3rd-party-api/src/main/java/com/nachiket/consume3rdpartyapi/Consume3rdPartyApiApplication.java
@@ -0,0 +1,13 @@
+package com.nachiket.consume3rdpartyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consume3rdPartyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Consume3rdPartyApiApplication.class, args);
+	}
+
+}
---
+++ consume-3rd-party-api/src/test/java/com/nachiket/consume3rdpartyapi/Consume3rdPartyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nachiket.consume3rdpartyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Consume3rdPartyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
