d56f0f2dcbacf6782a8d568ff1e634502d182caa
mintifi <nachiketkhule.nk@gmail.com>
2023-06-09 15:25:28 +0530

    wip

---
+++ Spring Boot Cloud/config client-first/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+K
+config-client-first/pom.xml,7/6/761e40aeafa2b917b54d118c9d614189cdb766be
+‰
+Yconfig-client-first/src/main/java/com/nachiket/config/clientfirst/GreetingController.java,9/a/9ad51325521f83ad80613e76db65425e0b01e0ef
+“
+cconfig-client-first/src/main/java/com/nachiket/config/clientfirst/ConfigClientFirstApplication.java,c/3/c3184884bdbc5a70f11dfb56c073f1f2f283a6e9
--- Spring Boot Cloud/config client-first/config-client-first/src/main/java/com/nachiket/config/clientfirst/GreetingController.java
+++ Spring Boot Cloud/config client-first/config-client-first/src/main/java/com/nachiket/config/clientfirst/GreetingController.java
@@ -4,14 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 @RestController
 @RequestMapping("/test")
+@RequestScope
 public class GreetingController {
   @Value("${docs.uri}")
   private String url;
   @GetMapping("/")
   public String hello() {
+    System.out.println("Hello guys");
     System.out.println("url = " + url);
     return url;
   }
